2dfa85566fa3afda21e9ea8e53bdbf24de79083d
Sebin <sebinsuresh@gmail.com>
2019-10-12 13:38:13 -0500

    Added Dither.java

---
+++ Dithering-Java/Dither.java
@@ -0,0 +1,15 @@
+/* 
+ * Sebin Puthenthara Suresh
+ * github.com/sebinsuresh
+ * A Java program that produces dithered images using some JavaFX classes.
+ * Output may be exported as an image file or printed in the console, given the width of the console in number of chars.
+ * Usage:
+ * 
+ */
+
+
+ public class Dither {
+     public static void main (String[] args){
+         
+     }
+ }
