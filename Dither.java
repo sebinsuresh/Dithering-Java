29a3d7331b04f84f57254ad13abf4dc20309abd9
Sebin <sebinsuresh@gmail.com>
2019-10-13 16:36:38 -0500

    Improved dithering algorithm, the error is a double variable now that ensures that the decimal error values are diffused to the nearby pixels.

--- Dither.java
+++ Dither.java
@@ -8,6 +8,7 @@
  */
 import java.io.File;
 import java.awt.image.BufferedImage;
+import java.awt.Color;
 import javax.imageio.ImageIO;
 
 public class Dither {
@@ -33,91 +34,101 @@ public class Dither {
         int width = img.getWidth();
         System.out.println(filename + " " + width + "x" + height + " " + img.getType());
         
+        // BufferedImage output = new BufferedImage(width, height, BufferedImage.TYPE_INT_ARGB);
         BufferedImage output = new BufferedImage(width, height, img.getType());
+        
+        // Sets all alpha = 0 pixels to white color & convert output to greyscale
         for(int i=0; i < height; i++){
             for(int j = 0; j < width; j++){
                 int color = img.getRGB(j,i);
-                int alpha = (color&0xff000000)>>24;
+                int alpha = (color&0xff000000)>>24; 
+                color = getGSRGBfromGrey(getGreyFromRGB(color)); // convert color to greyscale value
                 if(alpha == 0){
-                    img.setRGB(j,i,0xffffffff);
+                    // img.setRGB(j,i,0xffffffff);
+                    output.setRGB(j,i,0xffffffff);
+                } else {
+                    output.setRGB(j,i,color);
                 }
             }
         }
+
         for(int i=0; i < height; i++){
             for(int j = 0; j < width; j++){
                 // The getRGB method returns RGBA values in the default TYPE_INT_ARGB type.
                 // This gives 8 bit RGBA color components packed into one integer, in the order Alpha,Red,Green,Blue
                 // Eg: 00000000 00000100 00000100 00000100 -> (r,g,b,a) = (4,4,4,0) -> you get 1092 in decimal int.
                 // I'll use bit masking & shifting to get individual values.
-                int color = img.getRGB(j,i);
+                int color = output.getRGB(j,i);
                 
-                int greyValue = getGreyFromRGB(color);
+                //Get the blue component which should be equal to the greyscale value, since all pixels are converted to greyscale earlier
+                int greyValue = color & 0xff; 
 
-                int newColor = 0; 
+                int newColor = 0;
+                // Error needs to be double/float because the decimal points matter when the error is diffused to nearby pixels 
                 double error = 0;
                 if(greyValue < 128){
-                    error = (greyValue)/42;
+                    error = (double)greyValue/42.0;
                     newColor = 0xff000000; //Black
                 } else {
-                    error = (greyValue-255)/42;
+                    error = (double)(greyValue-255)/42;
                     newColor = 0xffffffff; //white
                 }
 
                 //Same row
                 if(j < width-1){ //there is a column to right
-                    int neighbor = getGreyFromRGB(img.getRGB(j+1,i));
-                    img.setRGB(j+1,i, getGSRGBfromGrey(neighbor + (error*8))); //error * 8
+                    int neighbor = (output.getRGB(j+1,i))&0xff;
+                    output.setRGB(j+1,i, getGSRGBfromGrey(neighbor + (error*8))); //error * 8
                 }
                 if(j < width-2){ //there are two columns to the right
-                    int neighbor = getGreyFromRGB(img.getRGB(j+2,i));
-                    img.setRGB(j+2,i, getGSRGBfromGrey(neighbor + (error*4))); //error * 4
+                    int neighbor = (output.getRGB(j+2,i))&0xff;
+                    output.setRGB(j+2,i, getGSRGBfromGrey(neighbor + (error*4))); //error * 4
                 }
                 
                 //there is a row below
                 if(i < height-1){
-                    int neighbor = getGreyFromRGB(img.getRGB(j,i+1));
-                    img.setRGB(j,i+1, getGSRGBfromGrey(neighbor + (error*8))); //error * 8
+                    int neighbor = (output.getRGB(j,i+1))&0xff;
+                    output.setRGB(j,i+1, getGSRGBfromGrey(neighbor + (error*8))); //error * 8
 
                     if(j < width-1){ //there is a column to right
-                        neighbor = getGreyFromRGB(img.getRGB(j+1,i+1));
-                        img.setRGB(j+1,i+1, getGSRGBfromGrey(neighbor + (error*4))); //error * 4
+                        neighbor = (output.getRGB(j+1,i+1))&0xff;
+                        output.setRGB(j+1,i+1, getGSRGBfromGrey(neighbor + (error*4))); //error * 4
                     }
                     if(j < width-2){ //there are two columns to the right
-                        neighbor = getGreyFromRGB(img.getRGB(j+2,i+1));
-                        img.setRGB(j+2,i+1, getGSRGBfromGrey(neighbor + (error*2))); //error * 2
+                        neighbor = (output.getRGB(j+2,i+1))&0xff;
+                        output.setRGB(j+2,i+1, getGSRGBfromGrey(neighbor + (error*2))); //error * 2
                     }
 
                     if(j > 0){ //there is a column to left
-                        neighbor = getGreyFromRGB(img.getRGB(j-1,i+1));
-                        img.setRGB(j-1,i+1, getGSRGBfromGrey(neighbor + (error*4))); //error * 4
+                        neighbor = (output.getRGB(j-1,i+1))&0xff;
+                        output.setRGB(j-1,i+1, getGSRGBfromGrey(neighbor + (error*4))); //error * 4
                     }
                     if(j > 1){ //there are two columns to the left
-                        neighbor = getGreyFromRGB(img.getRGB(j-2,i+1));
-                        img.setRGB(j-2,i+1, getGSRGBfromGrey(neighbor + (error*2))); //error * 2
+                        neighbor = (output.getRGB(j-2,i+1))&0xff;
+                        output.setRGB(j-2,i+1, getGSRGBfromGrey(neighbor + (error*2))); //error * 2
                     }
                 }
 
                 //there are two rows below
                 if(i < height-2){ 
-                    int neighbor = getGreyFromRGB(img.getRGB(j,i+2));
-                    img.setRGB(j,i+2, getGSRGBfromGrey(neighbor + (error*4))); // error * 4
+                    int neighbor = (output.getRGB(j,i+2))&0xff;
+                    output.setRGB(j,i+2, getGSRGBfromGrey(neighbor + (error*4))); // error * 4
 
                     if(j < width-1){ //there is a column to right
-                        neighbor = getGreyFromRGB(img.getRGB(j+1,i+2));
-                        img.setRGB(j+1,i+2, getGSRGBfromGrey(neighbor + (error*2))); //error * 2
+                        neighbor = (output.getRGB(j+1,i+2))&0xff;
+                        output.setRGB(j+1,i+2, getGSRGBfromGrey(neighbor + (error*2))); //error * 2
                     }
                     if(j < width-2){ //there are two columns to the right
-                        neighbor = getGreyFromRGB(img.getRGB(j+2,i+2));
-                        img.setRGB(j+2,i+2, getGSRGBfromGrey(neighbor + error)); //error * 1
+                        neighbor = (output.getRGB(j+2,i+2))&0xff;
+                        output.setRGB(j+2,i+2, getGSRGBfromGrey(neighbor + error)); //error * 1
                     }
 
                     if(j > 0){ //there is a column to left
-                        neighbor = getGreyFromRGB(img.getRGB(j-1,i+2));
-                        img.setRGB(j-1,i+2, getGSRGBfromGrey(neighbor + (error*2))); //error * 2
+                        neighbor = (output.getRGB(j-1,i+2))&0xff;
+                        output.setRGB(j-1,i+2, getGSRGBfromGrey(neighbor + (error*2))); //error * 2
                     }
                     if(j > 1){ //there are two columns to the left
-                        neighbor = getGreyFromRGB(img.getRGB(j-2,i+2));
-                        img.setRGB(j-2,i+2, getGSRGBfromGrey(neighbor + error)); //error * 1
+                        neighbor = (output.getRGB(j-2,i+2))&0xff;
+                        output.setRGB(j-2,i+2, getGSRGBfromGrey(neighbor + error)); //error * 1
                     }
                 }
 
@@ -148,9 +159,11 @@ public class Dither {
     // Returns a Greyscale RGB equivalent of a greyscale value between 0-255
     // In TYPE_INT_ARGB format
     public static int getGSRGBfromGrey(int greyscale){
+        //greyscale = greyscale&0xffffff;
         return (0xff<<24)+(greyscale<<16)+(greyscale<<8)+greyscale;
     }
     public static int getGSRGBfromGrey(double greyscale){
-        return getGSRGBfromGrey((int)greyscale);
+        int gs = (int)Math.round(greyscale);
+        return getGSRGBfromGrey(gs);
     }
  }
